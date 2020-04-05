d0e8d4bc1028becf4812a5d74e439fbe0cd15fa1
Gean Carlos <gean.farias@outlook.com>
2020-04-05 20:07:16 -0300

    Configurando botão twitter
    configurando o botão de compartilhar no twitter e finalizando alguns detalhes.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
-const newOption = (then) => {
-    $.get('http://localhost:3000/sorter.php', then);
+const newOption = () => {
+    $.when(
+        $.get('http://localhost:3000/sorter.php')
+    )
+        .done(function (response) {
+            createCard(response);
+        });
 };
 
-$(function() {
-    $("#name").keyup(function(){
-        if ($(this).val().length >= 3){
-            $("#Sortear").prop("disabled", false);
+$(function () {
+    $("#name").keyup(function () {
+        if ($(this).val().length >= 3) {
+           return $("#Sortear").prop("disabled", false);
         }
+        return $("#Sortear").prop("disabled", true);
     });
+
+    $("#Sortear").click(() => {
+        newOption();
+    });
+
+    createCard = (response) => {
+        $("#WhatToDo").html($("#name").val() + ", " +response);
+        $(".box").fadeIn("fast");
+        $("#ttButton").prop("href", "https://twitter.com/intent/tweet?button_hashtag=Quarenteners&text=" + response + "%0A");
+    };
 });
